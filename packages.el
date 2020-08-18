08a67f09e5d597c6a404f74eb40a7f56aa0dae52
lytex <julianlpc15@gmail.com>
2020-08-18 20:54:27 +0200

    add org-journal and config

--- packages.el
+++ packages.el
@@ -16,6 +16,8 @@
 ;  :recipe (:host github :repo "username/repo"))
 (package! org-bullets 
     :recipe (:host github :repo "sabof/org-bullets"))
+(package! org-journal 
+    :recipe (:host github :repo "bastibe/org-journal"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
