e0d5cfde01dd1133af8d9e47e59dae66a9be4d72
lytex <julianlpc15@gmail.com>
2020-08-18 19:27:48 +0200

    add org-bullets

--- packages.el
+++ packages.el
@@ -14,6 +14,8 @@
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
+(package! org-bullets 
+    :recipe (:host github :repo "sabof/org-bullets"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
