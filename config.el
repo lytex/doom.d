38fdeb52742acd7e7d4263e2c73ef7dc7538f1e9
lytex <julianlpc15@gmail.com>
2020-08-20 02:00:25 +0200

    fix wrong IDEA state

--- config.el
+++ config.el
@@ -80,7 +80,7 @@
  )
 (after! org
 (setq org-todo-keywords
-	'((sequence "IDEA(i) TODO(t)" "NEXT(n)" "BLOCK(b)" "ONGOING(o)" "TICKLER(t)" "|" "DONE(d)")))
+	'((sequence "IDEA(i)" "TODO(t)" "NEXT(n)" "BLOCK(b)" "ONGOING(o)" "TICKLER(k)" "|" "DONE(d)")))
 (setq org-priority-highest ?A)
 (setq org-priority-lowest ?F)
 (setq org-default-priority ?E)
@@ -93,13 +93,23 @@
       :custom
       (org-roam-directory "~/org")
       :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
+              (
                ("C-c n f" . org-roam-find-file)
                ("C-c n j" . org-roam-jump-to-index)
                ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph))
               :map org-mode-map
-              (("C-c n i" . org-roam-insert))))
+              (("C-c n i" . org-roam-insert)))))
+;; SPC is implicitly included
+(map! :leader
+      :map org-roam-mode-map
+      :desc (documentation 'org-roam) "ro" #'org-roam)
+(map! :leader
+      :map org-roam-mode-map
+      :desc (documentation 'org-roam) "rg" #'org-roam-graph)
+(map! :leader
+      :map org-roam-mode-map
+      :desc (documentation 'org-roam-capture) "rc" #'org-roam-capture)
+
 
 (use-package org-journal
   :custom
@@ -110,7 +120,7 @@
 
 ;; SPC is implicitly included
 (map! :leader
-      :desc "Open today's journal file and start a new entry." "mj" #'org-journal-new-entry)
+      :desc (documentation 'org-journal-new-entry)  "mj" #'org-journal-new-entry)
 (setq org-default-notes-file (concat org-directory "/Inbox.org"))
 (setq org-capture-templates
       '(("t" "Todo" entry (file "~/org/Inbox.org" )
