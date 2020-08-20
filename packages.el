368f30aa7a85ecf32e1b023aae265f0715af99f1
lytex <julianlpc15@gmail.com>
2020-08-20 15:12:39 +0200

    add ejira, separate branch for work stuff

--- packages.el
+++ packages.el
@@ -18,6 +18,8 @@
     :recipe (:host github :repo "sabof/org-bullets"))
 (package! org-journal 
     :recipe (:host github :repo "bastibe/org-journal"))
+(package! ejira
+    :recipe (:host github :repo "nyyManni/ejira"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
