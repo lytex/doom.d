8bb54b9b942f508d317ac225b3aacff241788ae1
lytex <julianlpc15@gmail.com>
2020-08-26 22:39:36 +0200

    use re.sub instead of manual replacement

--- replace_custom_ids.py
+++ replace_custom_ids.py
@@ -3,12 +3,17 @@ import re
 from glob import glob
 from typing import Callable, Iterable
 from uuid import uuid4
+import os
 
-from orgparse import load
+from orgparse import loads
 from orgparse.node import OrgRootNode
 
+ORG_DIRECTORY = os.environ.get("ORG_DIRECTORY")
 
-def recursive_filter(condition: Callable[[OrgRootNode], bool], root: Iterable[OrgRootNode]) -> Iterable[OrgRootNode]:
+
+def recursive_filter(
+    condition: Callable[[OrgRootNode], bool], root: Iterable[OrgRootNode]
+) -> Iterable[OrgRootNode]:
     """recursively trasvese all possible nodes from root and return only those for which
         condition returns True
 
@@ -24,7 +29,8 @@ def recursive_filter(condition: Callable[[OrgRootNode], bool], root: Iterable[Or
         if node.children:
             yield from recursive_filter(condition, node.children)
 
-def get_children(parent):
+
+def get_children(parent: OrgRootNode) -> Iterable[OrgRootNode]:
     if parent.children:
         for node in parent.children:
             yield node
@@ -32,41 +38,41 @@ def get_children(parent):
                 yield from get_children(node)
 
 
-# r = "\n".join([str(x) for x in root])
-# with open("test.org", "w") as f:
-#     f.write(r)
-
 # This dictionary maps each custom_id to their id (either existent id or newly generated id)
-custom_to_id = {} 
+custom_to_id = {}
 
-for path in glob("/home/julian/org/**/*.org", recursive=True):
-    root = load(path)
-    custom_id = recursive_filter(lambda x: x.properties.get('custom_id') is not None, get_children(root))
+for path in glob(f"{ORG_DIRECTORY}/**/*.org", recursive=True):
+    with open(path) as f:
+        root = loads(f.read())
+
+    custom_id = recursive_filter(
+        lambda x: x.properties.get("custom_id") is not None, get_children(root)
+    )
 
     for item in custom_id:
-        uuid = item.properties.get('ID', str(uuid4())) # Create id if not exists only
-        custom_to_id.update({item.properties['custom_id']: uuid})
-        item.properties.update({'ID': uuid})
+        uuid = item.properties.get("ID", str(uuid4()))  # Create id if not exists only
+        custom_to_id.update({item.properties["custom_id"]: uuid})
+        item.properties.update({"ID": uuid})
 
-for path in glob("/home/julian/org/**/*.org", recursive=True):
-    with open(path, "w") as f:
+for path in glob(f"{ORG_DIRECTORY}/**/*.org", recursive=True):
+    with open(path, "r+") as f:
+
         content = f.read()
 
         for custom, uuid in custom_to_id.items():
-            # Match simple links [[#link]]
-            matches = re.findall(r"\[\[#"+custom+"\]\]", content)
-            for match in matches:
-                content = content.replace(match, f"[[id:{uuid}[{custom}]]")
 
-            # Match links with names [[#link][name]]
-            matches = re.findall(r"\[\[#"+custom+"\]\[[ \w\d-]+\]\]", content)
-            for match in matches:
-                content = content.replace(match, f"[[id:{uuid}[{custom}]]")
+            # Substitute simple links [[#link]]
+            content = re.sub(
+                r"\[\[#" + custom + "\]\]", f"[[id:{uuid}][{custom}]]", content
+            )
 
+            # Substitute links with names [[#link][name]]
+            content = re.sub(
+                r"\[\[#" + custom + "\]\[([ \w\d-]+)\]\]",
+                "[[id:" + uuid + r"][\1]]",
+                content,
+            )
+
+        # Overwrite content
+        f.seek(0)
         f.write(content)
-
-        
-
-
-
-            
