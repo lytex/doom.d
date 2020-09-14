c9c7d3e3fba403a94379987bf12db338e37e9f49
lytex <julianlpc15@gmail.com>
2020-09-15 01:34:39 +0200

    merge custom.el

--- custom.el
+++ custom.el
@@ -3,13 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-<<<<<<< HEAD
  '(package-selected-packages (quote (dash)))
-=======
- '(package-selected-packages
-   (quote
-    (esup helm-swoop zygospore use-package url-http-ntlm org-roam-server org-ql org-plus-contrib org-journal neotree nadvice helm-org-rifle fsm fira-code-mode)))
->>>>>>> e44309a5771f3040f672af94680323f0e2877530
  '(safe-local-variable-values (quote ((eval require (quote org-roam-dev))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
