7d63788d06842a6a5e751584f573b4dbc2f11890
lytex <julianlpc15@gmail.com>
2020-09-30 19:55:18 +0200

    wip script to create links to org files

---
+++ link_to_org_folder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+prefix="$HOME"
+declare -A org_links
+org_links["$HOME/Documents"]="$HOME/org/roam/"
+org_links["$HOME/code"]="$HOME/org/projects"
+
+for source_dir in ${!org_links[@]}; do
+    readarray -t files <<< $(find "$source_dir" -iname "*.org")
+    for ((i=0; i<${#files[@]}; i++)); do
+        file=${files[i]}
+        pushd ${org_links[$source_dir]} >> /dev/null
+            echo "ln -s $file"
+        popd >> /dev/null
+    done
+done
