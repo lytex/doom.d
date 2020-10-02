9304bea20e1e40510cc2f92da2d16782b5b0f5cd
lytex <julianlpc15@gmail.com>
2020-10-03 00:07:57 +0200

    hard links and remove bad condition

--- link_to_org_folder.sh
+++ link_to_org_folder.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-prefix="$HOME"
 declare -A org_links
 org_links["$HOME/Documents"]="$HOME/org/roam/"
 org_links["$HOME/code"]="$HOME/org/projects"
@@ -10,9 +9,11 @@ for source_dir in ${!org_links[@]}; do
     for ((i=0; i<${#files[@]}; i++)); do
         file=${files[i]}
         pushd ${org_links[$source_dir]} >> /dev/null
-            if [ ! -f "$file" ]; then
-                ln -s "$file"
-            fi
+            ln "$file"  # It has to be a hard link
+            # If do a soft link Documents -> org, then org-noter doesn't work
+            # because it cannot open the .org file associated with the .pdf  properly
+            # If do a soft link org -> Documents, then org-roam doesn't work
+            # because it cannot create a new roam file if you write a non-existing link
         popd >> /dev/null
     done
 done
