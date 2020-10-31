ca716687198da69a65210812b75fc7d24e5d0088
lytex <julianlpc15@gmail.com>
2020-10-31 14:15:27 +0100

    add more utils to get all tags

--- utils/rename_tags.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-old_tag="myoldtag"; new_tag="mynewtag"; sed -Ei "s/:$old_tag:/:$new_tag:/g" $(grep -rl ":$old_tag:" * --exclude-dir=Documents --exclude-dir=code --binary-files=without-match)
