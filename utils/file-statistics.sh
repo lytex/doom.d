79289ed3278321a2381e6d1642b7914a4d11a047
lytex <julianlpc15@gmail.com>
2020-11-22 14:02:06 +0100

    add bash scripts to get file statistics

---
+++ utils/file-statistics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Number of total org files
+find . -iname "*.org" | wc -l
+
+# Create a csv file with information about each file
+now="`date +'%Y-%m-%d %H:%M:%S'`"
+echo "lines,words,bytes,path" > "statistics $now.csv"
+find . -iname "*.org" -exec bash -c 'wc "{}"' \; | \
+sed -E 's/[ ]*([0-9]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+/\1,\2,\3,/g' >> "statistics $now.csv"
+
+# Create a csv file with information about each file (sorted desc by lines)
+now="`date +'%Y-%m-%d %H:%M:%S'`"
+echo "lines,words,bytes,path" > "statistics $now.csv"
+find . -iname "*.org" -exec bash -c 'wc "{}"' \; | \
+sed -E 's/[ ]*([0-9]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+/\1,\2,\3,/g' | sort -nr >> "statistics $now.csv"
