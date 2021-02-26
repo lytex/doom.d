5deb9bd6428123db9e68003abf6e697e20832fc0
lytex <julianlpc15@gmail.com>
2021-02-26 20:58:06 +0100

    add SETUPFILE to templates

--- init.el
+++ init.el
@@ -21,12 +21,25 @@
   (setq evil-move-cursor-back nil)
   (setq evil-want-fine-undo t))
 
+
+(defun my/ox-html-format-drawer (name content backend)
+  "Export :NOTES: and :LOGBOOK: drawers to HTML class"
+  (cond
+    ((string-match "RELATED\\|DEPENDS\\|TAGS" name)
+    (cond
+      ((eq backend 'html)
+      (format "@<div class=\".org-org-drawer\"> %s @</div>" content))
+      (t nil)))
+    (t nil)))
+
+
 (use-package-hook! org
   :pre-init
   (setq org-html-head-extra
 "<style> #content{max-width:80%;}</style>
 <style> p{max-width:100%;}</style>
-<style> li{max-width:100%;}</style>"))
+<style> li{max-width:100%;}</style>")
+  (setq org-html-format-drawer-function 'my/ox-html-format-drawer))
 
 (doom! :input
        ;;chinese
