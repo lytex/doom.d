b097fde450dd8bc7691ca03509d055819d869b9f
lytex <julianlpc15@gmail.com>
2021-02-27 21:58:38 +0100

    disable org-transclusion in undo/redo and org-meta↑←↓→

--- modules/org-transclusion.el
+++ modules/org-transclusion.el
@@ -9,3 +9,10 @@
 
 (with-eval-after-load 'org
   (define-key global-map (kbd "<f12>") #'org-transclusion-mode))
+
+(advice-add 'evil-undo :before (lambda (count) (if org-transclusion-mode (org-transclusion-deactivate))))
+(advice-add 'evil-redo :before (lambda (count) (if org-transclusion-mode (org-transclusion-deactivate))))
+(advice-add 'org-metaup :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
+(advice-add 'org-metaleft :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
+(advice-add 'org-metadown :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
+(advice-add 'org-metaright :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
