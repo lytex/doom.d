2ad8963ab9dd51d273888c7eb843d254a3372eba
lytex <julianlpc15@gmail.com>
2021-03-19 23:23:59 +0100

    enable read-only when org-transclusion is active

--- modules/org-transclusion.el
+++ modules/org-transclusion.el
@@ -7,11 +7,16 @@
 (setq make-backup-files nil)
 (setq org-transclusion-include-first-section nil)
 
+(defun my/org-transclusion-read-only (&optional arg)
+(if org-transclusion-mode
+  (setq buffer-read-only t)
+  (setq buffer-read-only nil)))
+
 (with-eval-after-load 'org
   (define-key global-map (kbd "<f12>") #'org-transclusion-mode))
 
-(advice-add 'evil-undo :before (lambda (count) (if org-transclusion-mode (org-transclusion-deactivate))))
-(advice-add 'evil-redo :before (lambda (count) (if org-transclusion-mode (org-transclusion-deactivate))))
+(advice-add 'org-transclusion-mode :after 'my/org-transclusion-read-only)
+
 (advice-add 'org-metaup :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
 (advice-add 'org-metaleft :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
 (advice-add 'org-metadown :before (lambda (&optional arg) (if org-transclusion-mode (org-transclusion-deactivate))))
