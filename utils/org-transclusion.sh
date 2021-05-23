7037da3839bca9138e9aa3c1a5b9eeeb346dc818
lytex <julianlpc15@gmail.com>
2021-05-23 22:09:33 +0200

    add org-transclusion convert script

---
+++ utils/org-transclusion.sh
@@ -0,0 +1,1 @@
+find . -type f -iname "*.org" -exec sh -c 'grep -iFl "#+transclude" "{}"' \; | xargs -I _ sed -i -z 's/\(#+transclude: t\)[ ]\+\(:hlevel[ ]\+\([0-9]\)\)\?\n\([^\n]\+\)/\1 \4 :level \3/gi' "_"
