3c3da6e186c08c7a11003d5584c11ab630522bf3
lytex <julianlpc15@gmail.com>
2021-06-05 00:07:13 +0200

    reorder org-ql queries

--- modules/org-ql.el
+++ modules/org-ql.el
@@ -1,128 +1,6 @@
 (use-package! org-ql)
 (use-package! org-ql-search)
 
-(defun oql/refile-not-inbox ()
-"REFILE entries not in Inbox"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and
-    (todo "REFILE")
-    (not
-     (tags "inbox")))))
-
-(defun oql/today ()
-"sched for today"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo) (not (todo "MAYBE" "SOMEDAY")) (scheduled 1))
-  :sort 'scheduled))
-
-(defun oql/next-3-days ()
-"sched for next 3 days"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo) (not (todo "MAYBE" "SOMEDAY")) (not (tags "tareas" "habits" "mantenimiento")) (scheduled 3))
-  :sort 'scheduled))
-
-(defun oql/week ()
-"sched for next 7 days"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo) (not (todo "MAYBE" "SOMEDAY")) (not (tags "tareas" "habits" "mantenimiento")) (scheduled 7))
-  :sort 'scheduled))
-
-(defun oql/todo ()
-"todo itemss"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo) (not (todo "MAYBE" "SOMEDAY")) (not (tags "tareas" "habits" "mantenimiento")))))
-
-(defun oql/ongoing ()
-"ongoing items"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo "ONGOING") (not (tags "work"))))) 
-
-(defun oql/ongoing-projects ()
-"ongoing projects that are items"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo "ONGOING") (not (tags "work")) (ltags "project")))) 
-
-
-(defun oql/ongoing-left ()
-"ongoing items that are not projects"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo "ONGOING") (not (ltags "project"))))) 
-
-(defun oql/work ()
-"work items"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo) (tags "work"))))
-
-(defun oql/research ()
-"items not done and not archived with tag research"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (not (todo)) (not (tags "ARCHIVE")) (ltags "research"))))
-
-(defun oql/learn ()
-"items not done and not archived with tag learn or tag indepth"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (not (todo)) (not (tags "ARCHIVE")) (ltags "learn" "indepth"))))
-
-(defun oql/try ()
-"items not done and not archived with tag try"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (not (todo)) (not (tags "ARCHIVE")) (ltags "try")))) 
- 
-(defun oql/indepth ()
-"items not done and not archived with tag indepth"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (not (todo)) (not (tags "ARCHIVE")) (ltags "indepth"))))
- 
-(defun oql/refile ()
-"refile items"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo "REFILE") (not (tags "work")))))
-
-(defun oql/verify ()
-"verify items"
-(interactive)
-(org-ql-search
-  (org-agenda-files)
-  '(and (todo "VERIFY") (not (tags "work")))))
-
-(defun oql/templates ()
-"Entries with local tag \"template\""
-(interactive)
-(org-ql-search (org-agenda-files) '(ltags "template")))
-
-(defun oql/habits ()
-"Entries with local tag \"habits\""
-(interactive)
-(org-ql-search (org-agenda-files) '(or (ltags "habit") (property "STYLE" "habit"))))
-
 (setq org-super-agenda-groups '((:auto-planning)))
 
 
@@ -165,6 +43,16 @@
          :query (and (todo "ONGOING") (not (tags "work")) (ltags "project"))
          :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "")
 
+        ("Refile" :buffers-files
+         org-agenda-files
+         :query (and (todo "REFILE") (not (tags "work")))
+         :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "Refile")
+
+        ("Refile not Inbox" :buffers-files
+         org-agenda-files
+         :query (and (todo "REFILE") (not (tags "inbox")))
+         :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "Refile not Inbox")
+
         ("OnGoing Left" :buffers-files
          org-agenda-files
          :query (and (todo "ONGOING") (not (ltags "project")))
@@ -195,11 +83,6 @@
          :query (and (not (todo)) (not (tags "ARCHIVE")) (ltags "indepth"))
          :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "In Depth")
 
-        ("Refile" :buffers-files
-         org-agenda-files
-         :query (and (todo "REFILE") (not (tags "work")))
-         :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "Refile")
-
         ("Verify" :buffers-files
          org-agenda-files
          :query (and (todo "VERIFY") (not (tags "work")))
@@ -209,16 +92,6 @@
          org-agenda-files
          :query (ltags "template")
          :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "Templates")
-
-        ("Refile not Inbox" :buffers-files
-         org-agenda-files
-         :query (and (todo "REFILE") (not (tags "inbox")))
-         :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "Refile not Inbox")
-
-        ("" :buffers-files
-         org-agenda-files
-         :query
-         :sort (date) :narrow nil :super-groups org-super-agenda-groups :title "")
 
         ("Habits" :buffers-files
          org-agenda-files
