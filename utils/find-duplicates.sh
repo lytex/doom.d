d7d18e6cb8982466f8dd1f5da817abf0856c1c30
lytex <julianlpc15@gmail.com>
2021-06-08 10:38:29 +0200

    update find duplicate util

--- utils/find-duplicates.sh
+++ utils/find-duplicates.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Find duplicates in id
-rg ":ID:" projects/emacs.org | sort | uniq -c | grep "[2-9][0-9]* :ID:" 
+# rg ":ID:" projects/emacs.org | sort | uniq -c | grep "[2-9][0-9]* :ID:" 
+rg ":ID:" File.org archive/File_Archive.org | sort | uniq -c | grep "^[ ]*[2-9][0-9]*"
