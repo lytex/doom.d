3ff32ba87e2ae318ed65856966b8e071bcbcc643
lytex <julianlpc15@gmail.com>
2021-06-19 14:34:39 +0200

    add script to get id dupliacates in any files

--- utils/find-duplicates.sh
+++ utils/find-duplicates.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# Find duplicates in id
-# rg ":ID:" projects/emacs.org | sort | uniq -c | grep "[2-9][0-9]* :ID:" 
+# Find duplicates in one file
+rg ":ID:" projects/emacs.org | sort | uniq -c | grep "[2-9][0-9]* :ID:" 
+# Find duplicates in two files
 rg ":ID:" File.org archive/File_Archive.org | sort | uniq -c | grep "^[ ]*[2-9][0-9]*"
+# Find duplicates in any files
+cat ~/1.txt | xargs -I _ rg -I "_" | sort | uniq -c | grep -E "[ ]*[2-9][0-9]* :ID:"  
