3c629f17d79d88a3958cf4043a0809e98371fdc6
lytex <julianlpc15@gmail.com>
2021-10-10 14:55:37 +0200

    fix org-journal entry hook to insert CREATED

--- modules/org-journal.el
+++ modules/org-journal.el
@@ -3,22 +3,19 @@
 (defun lytex/set-work-journal ()
         (setq org-journal-dir (concat org-directory "work_journal/"))
         ;; Workaround to work with orgzly -> each file must have a unique name
-        (setq orgzly-org-journal-file-format "%Y-%m-%dW.org")
-        (setq org-journal-file-format orgzly-org-journal-file-format)
+        (setq org-journal-file-format "%Y-%m-%dW.org")
         ;; All work journal files have the tag "work"
         (setq org-journal-file-header "#+TITLE:%Y-%m-%dW\n#+FILETAGS: :work:")
         (setq org-journal-date-format "%A, %d de %B de %Y :work:"))
 
 (defun lytex/set-personal-journal ()
         (setq org-journal-dir (concat org-directory "journal/"))
-        (setq orgzly-org-journal-file-format "%Y-%m-%d.org")
-        (setq org-journal-file-format orgzly-org-journal-file-format)
+        (setq org-journal-file-format "%Y-%m-%d.org")
         (setq org-journal-file-header "#+TITLE: %Y-%m-%d")
         (setq org-journal-date-format "%A, %d de %B de %Y"))
 
 (defun lytex/set-introspection-journal ()
-        (setq orgzly-org-journal-file-format "%Y-%m-%dI.org")
-        (setq org-journal-file-format orgzly-org-journal-file-format)
+        (setq org-journal-file-format "%Y-%m-%dI.org")
         (setq org-journal-dir (concat org-directory "Introspección/"))
         (setq org-journal-file-header "#+TITLE: %Y-%m-%dI")
         (setq org-journal-date-format "%A, %d de %B de %Y"))
@@ -32,11 +29,10 @@
   (org-journal-date-prefix "* ")
   (if WORK_ENV
       (setq org-journal-date-format "%A, %d de %B de %Y :work:")
-      (setq org-journal-date-format "%A, %d de %B de %Y"))
-  (org-journal-file-format orgzly-org-journal-file-format)
-  (org-journal-after-entry-create-hook (lambda ()
-    (org-set-property "CREATED" (format-time-string "[%Y-%m-%d %a %H:%M]"))
-    (call-interactively 'org-close-fold))))
+      (setq org-journal-date-format "%A, %d de %B de %Y")))
+
+  (setq org-journal-after-entry-create-hook (lambda ()
+    (org-set-property "CREATED" (format-time-string "[%Y-%m-%d %a %H:%M]"))))
 
 (defun lytex/set-org-journal (option)
 (if (string= option "J")
