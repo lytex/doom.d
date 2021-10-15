7a2a1187018cd129e296de307c546035008000b7
lytex <julianlpc15@gmail.com>
2021-10-15 18:39:32 +0200

    add utils to count links

--- utils/file-statistics.sh
+++ utils/file-statistics.sh
@@ -17,3 +17,8 @@ sed -E 's/[ ]*([0-9]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+/\1,\2,\3,/g' | sort -nr >> "s
 
 # Count how many links of each website you have
 rg --no-filename -o "https?://[^/]+" | sed -E "s#https?://[^/]+\zs.*##" | sort | uniq -c | sort -nr | less
+
+# Count how many link to another notes there are (only id and file:... .org)
+now="`date +'%Y-%m-%d %H:%M:%S'`"
+echo "links,path" > "link statistics $now.csv"
+grep -Fc -e "[[id:" -e ".org]" *.org */*.org | sed -E 's/^([^:]+):(.+)/\2,\1/' | sort -nr >> "link statistics $now.csv"
