fb1bfa183d8d5193437bc94116379db422f514e5
lytex <julianlpc15@gmail.com>
2022-01-14 19:43:36 +0100

    add exchange_to_org script

---
+++ utils/exchange_to_org.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ORGANISED_EXCHANGE_DESTINATION="/home/julian/org/calendar.org"
+
+docker run \
+       --rm \
+       -v /home/julian/Work/calendar.ics:/calendar.ics \
+       ettomatic/organised-exchange:latest \
+       > $ORGANISED_EXCHANGE_DESTINATION
+
+sed -i -e "s/#+STARTUP: overview/&\n#+FILETAGS: :work:/ " "$ORGANISED_EXCHANGE_DESTINATION"
