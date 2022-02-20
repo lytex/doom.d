a90b8cc49e9c56eb10b05a01352ac9e11491d140
lytex <julianlpc15@gmail.com>
2022-02-20 10:21:54 +0100

    Add triple dots to fontifications

--- modules/pdf.el
+++ modules/pdf.el
@@ -75,7 +75,8 @@
   (replace-regexp "ﬃ" "ffi") 
   (replace-regexp "ﬁ" "fi") 
   (replace-regexp "ﬀ" "ff")
-  (replace-regexp "ﬂ" "fl"))
+  (replace-regexp "ﬂ" "fl")
+  (replace-regexp "…" "..."))
 
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (use-package! nov)
