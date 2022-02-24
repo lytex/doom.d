65bc4ad9c62cd3d228126a6c090fcc0c24eb9d48
lytex <julianlpc15@gmail.com>
2022-02-24 20:26:08 +0100

    add creation date script

--- utils/file-statistics.sh
+++ utils/file-statistics.sh
@@ -4,13 +4,13 @@
 find . -iname "*.org" | wc -l
 
 # Create a csv file with information about each file
-now="`date +'%Y-%m-%d %H:%M:%S'`"
+now="$(date +'%Y-%m-%d %H:%M:%S')"
 echo "lines,words,bytes,path" > "statistics $now.csv"
 find . -iname "*.org" -exec bash -c 'wc "{}"' \; | \
 sed -E 's/[ ]*([0-9]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+/\1,\2,\3,/g' >> "statistics $now.csv"
 
 # Create a csv file with information about each file (sorted desc by lines)
-now="`date +'%Y-%m-%d %H:%M:%S'`"
+now="$(date +'%Y-%m-%d %H:%M:%S')"
 echo "lines,words,bytes,path" > "statistics $now.csv"
 find . -iname "*.org" -exec bash -c 'wc "{}"' \; | \
 sed -E 's/[ ]*([0-9]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+/\1,\2,\3,/g' | sort -nr >> "statistics $now.csv"
@@ -19,6 +19,16 @@ sed -E 's/[ ]*([0-9]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+/\1,\2,\3,/g' | sort -nr >> "s
 rg --no-filename -o "https?://[^/]+" | sed -E "s#https?://[^/]+\zs.*##" | sort | uniq -c | sort -nr | less
 
 # Count how many link to another notes there are (only id and file:... .org)
-now="`date +'%Y-%m-%d %H:%M:%S'`"
+now="$(date +'%Y-%m-%d %H:%M:%S')"
 echo "links,path" > "link statistics $now.csv"
 grep -Fc -e "[[id:" -e ".org]" *.org */*.org | sed -E 's/^([^:]+):(.+)/\2,\1/' | sort -nr >> "link statistics $now.csv"
+
+
+now="$(date +'%Y%m%d%H%M%S')"
+echo "links,path" > "creation_date_$now.csv"
+find -iname "*.org" -exec /bin/bash -c 'echo $(git --no-pager log --diff-filter=A --follow --format=%aI -1 -- {}),{}' \; >> "creation_date_$now.csv"
+
+
+now="$(date +'%Y%m%d%H%M%S')"
+echo "links,path" > "creation_date_$now.csv"
+find -iname "*.org" -exec /bin/bash -c 'echo $(git --no-pager log --diff-filter=A --follow --format=%aI -1 -- {}),{}' \; | sort -nr >> "creation_date_$now.csv"
