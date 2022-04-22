1ad2c302d9eec54e33abd9371aab36c852d85271
lytex <julianlpc15@gmail.com>
2022-04-22 21:02:50 +0200

    add colored output

--- export.sh
+++ export.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 for file in $(find . -iname "*.org"); do
+    echo "Exporting file"
+
+    echo "\033[1;34mExporting file $file...\033[0m"
     emacs --batch \
     -l "$HOME/.emacs.d/early-init.el" \
     -l "$HOME/.emacs.d/init.el" \
     --eval="(progn (find-file \"$file\") (setq org-export-babel-evaluate nil) (org-transclusion-mode t) (org-html-export-to-html))"
+    echo "\033[1;32mFile $file exported.\033[0m"
 done
