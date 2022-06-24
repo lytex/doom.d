c540fb9667292dba283ca7734e875f69ff9bc7ed
lytex <julianlpc15@gmail.com>
2022-06-24 20:46:32 +0200

    add habitica example

---
+++ habitica_example.el
@@ -0,0 +1,6 @@
+;;; habitica.el -*- lexical-binding: t; -*-
+
+(setq habitica-uid "xxxxxxxx-yyyy-zzzz-aaaa-bbbbbbbbbbbb")
+(setq habitica-token "cccccccc-dddd-eeee-ffff-gggggggggggg")
+
+(use-package! habitica)
