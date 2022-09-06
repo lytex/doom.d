7c8086fb9cc667418290dd5234413d0221f3058e
lytex <julianlpc15@gmail.com>
2022-09-06 22:19:54 +0200

    remove quick_emacs_capture.sh

--- emacs/quick_emacs_capture.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if pgrep emacsclient; then
-    # There is an open emacsclient, open in existing frame
-    emacsclient "$@"
-else
-    # There is no open emacsclient, create a new frame
-    emacsclient -a "" -c "$@"
-fi
