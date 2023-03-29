3a65f5fc3cf79c34902744222152beafd31baf00
lytex <julianlpc15@gmail.com>
2023-03-29 10:16:56 +0200

    add id and roam include to calendar.org

--- utils/exchange_to_org.sh
+++ utils/exchange_to_org.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ORGANISED_EXCHANGE_DESTINATION="/home/julian/org/calendar.org"
+ORGANISED_EXCHANGE_DESTINATION="/home/julian/org/work_journal/calendar.org"
 
 docker run \
        --rm \
@@ -7,4 +7,4 @@ docker run \
        ettomatic/organised-exchange:latest \
        > $ORGANISED_EXCHANGE_DESTINATION
 
-sed -i -e "s/#+STARTUP: overview/&\n#+FILETAGS: :work:\n#+TITLE: Outlook Calendar/" "$ORGANISED_EXCHANGE_DESTINATION"
+sed -i -e "s/#+STARTUP: overview/:PROPERTIES:\n:ID:       4e3bf895-68bb-4a93-a592-c6329581b6f0\n:ROAM_INCLUDE: t\n:END:\n&\n#+FILETAGS: :work:\n#+TITLE: Outlook Calendar/" "$ORGANISED_EXCHANGE_DESTINATION"
