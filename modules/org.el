55050e4a1f2e47b994b4cbe027ac4415bfee5c76
lytex <julianlpc15@gmail.com>
2023-06-30 19:49:17 +0200

    hide org-mode properties

--- modules/org.el
+++ modules/org.el
@@ -41,6 +41,7 @@
     (mapcar #'browse-url (mapcar #'lytex/format-url links))))
 
 
+
 (map!
         :after org
         :map org-mode-map
@@ -320,6 +321,44 @@
         `(,numerator ,denominator))))
 
 (add-hook 'org-checkbox-statistics-hook (function aggregate-org-cookies))
+
+
+;; https://www.reddit.com/r/emacs/comments/6tewyl/hide_properties_drawer/
+(defun org-cycle-hide-drawers (state)
+  "Re-hide all drawers after a visibility state change."
+  (when (and (derived-mode-p 'org-mode)
+             (not (memq state '(overview folded contents))))
+    (save-excursion
+      (let* ((globalp (memq state '(contents all)))
+             (beg (if globalp
+                    (point-min)
+                    (point)))
+             (end (if globalp
+                    (point-max)
+                    (if (eq state 'children)
+                      (save-excursion
+                        (outline-next-heading)
+                        (point))
+                      (org-end-of-subtree t)))))
+        (goto-char beg)
+        (while (re-search-forward org-drawer-regexp end t)
+          (save-excursion
+            (beginning-of-line 1)
+            (when (looking-at org-drawer-regexp)
+              (let* ((start (1- (match-beginning 0)))
+                     (limit
+                       (save-excursion
+                         (outline-next-heading)
+                           (point)))
+                     (msg (format
+                            (concat
+                              "org-cycle-hide-drawers:  "
+                              "`:END:`"
+                              " line missing at position %s")
+                            (1+ start))))
+                (if (re-search-forward "^[ \t]*:END:" limit t)
+                  (outline-flag-region start (point-at-eol) t)
+                  (user-error msg))))))))))
 
 ;;;;;;;;;;;;;; org mode id autocompletion in all org-ggenda files ;;;;;;;;;;;;;;
 ;; https://emacs.stackexchange.com/questions/12391/insert-org-id-link-at-point-via-outline-path-completion
