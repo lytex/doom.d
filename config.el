43f55143a72dcb588e6af049fe72d99120616ca4
lytex <julianlpc15@gmail.com>
2024-09-03 20:19:42 +0200

    (setq tex-fontify-script nil)

--- config.el
+++ config.el
@@ -266,6 +266,7 @@
         :desc (documentation 'org-anki-cloze-dwim) "RET" #'org-anki-cloze-dwim
         :desc (documentation 'org-anki-sync-entry) "s" #'org-anki-sync-entry
         :desc (documentation 'org-anki-sync-all) "S" #'org-anki-sync-all))
+ (setq tex-fontify-script nil)
     (use-package! highlight-indent-guides
       :hook (prog-mode . highlight-indent-guides-mode)
       :init
