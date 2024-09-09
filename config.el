f46f30a0923c2ea696875418725627e3b9a64ad2
lytex <julianlpc15@gmail.com>
2024-09-09 19:39:40 +0200

    font-latex-fontify-script

--- config.el
+++ config.el
@@ -267,6 +267,7 @@
         :desc (documentation 'org-anki-sync-entry) "s" #'org-anki-sync-entry
         :desc (documentation 'org-anki-sync-all) "S" #'org-anki-sync-all))
  (setq tex-fontify-script nil)
+ (setq font-latex-fontify-script nil)
     (use-package! highlight-indent-guides
       :hook (prog-mode . highlight-indent-guides-mode)
       :init
