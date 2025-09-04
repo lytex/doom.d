15f0538f8e3f7e35abaaa717ec814ad5f8f5a7ca
Julián López Carballal <julopezc@acciona.com>
2025-09-04 09:11:52 +0200

    unpin org-ql dependencies

--- packages.el
+++ packages.el
@@ -24,13 +24,13 @@
                 (:host github :repo "astahlman/ob-async"))
         ;; org-ql breaks in emacs28 because of transient
         ;; https://emacs.stackexchange.com/questions/75827/doom-emacs-error-running-hook-global-git-commit-mode-because-void-variable
-        (package! transient
-              :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
-              :recipe (:host github :repo "magit/transient"))
+        ;; (package! transient
+        ;;       :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+        ;;       :recipe (:host github :repo "magit/transient"))
 
-        (package! with-editor
-                  :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
-                  :recipe (:host github :repo "magit/with-editor"))
+        ;; (package! with-editor
+        ;;           :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+        ;;           :recipe (:host github :repo "magit/with-editor"))
         (package! helm-ag)
         (package! org-re-reveal :recipe
                 (:host gitlab :repo "oer/org-re-reveal"))
